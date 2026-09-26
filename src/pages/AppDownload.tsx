7fdca5d6bc3b243510ee7592fb27f9682923708d
arnyinson <arnyinson2516@email.com>
2026-09-27 00:32:36 +0800

    Update APK download link to latest build

--- src/pages/AppDownload.tsx
+++ src/pages/AppDownload.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { Salad, Dumbbell, ShieldCheck, LineChart, Trophy, ArrowRight } from 'lucide-react';
 import Logo from '../components/Logo';
 
-const APK_DOWNLOAD_URL = 'https://drive.google.com/uc?export=download&id=1asv8_FDdcP84Dus3hrmbuhRNcqC2gYxC';
+const APK_DOWNLOAD_URL = 'https://drive.google.com/uc?export=download&id=1IW41GwU3MYapIi_XhYcGIOkKxhMP4L52';
 
 const SCREENS = [
   { src: '/dashboard.jpg', label: 'Dashboard' },
